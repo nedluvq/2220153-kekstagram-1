4a0ba7c3b38c6869a4c0860480182870d69127df
nedluvq <vasav7001@gmail.com>
2022-09-18 16:46:03 +0500

    module2-task1
    -randomNumber func
    -checkStringLength func

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function randomNumber(from,to) {
+  if(from < 0 || to < 0) {
+    return "Одна из границ отрицательная"
+  }
+  if(from > to) {
+    return Math.random(to,from);
+  }
+  return Math.random(from,to);
+}
+
+function checkStringLength(checkingString,maxLength ) {
+  if(checkingString.length <= maxLength ) {
+    return true
+  }else{
+    return false
+  }
+}
