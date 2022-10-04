753a09cbab62afb1f3c5c0cae2e1b5d24e2ac114
nedluvq <vasav7001@gmail.com>
2022-10-04 17:10:32 +0500

    4.15
    fixed

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ function getRandomPositiveInteger (a, b) {
 }
 
 function checkStringLength (checkingString, maxLength) {
-  return checkingString.length <= maxLength
+  return checkingString.length <= maxLength;
 
 }
 
 const generateArray = (length, max) => (
   [...new Array(length)]
-    .map(() => Math.round(Math.random() * max)))
+    .map(() => Math.round(Math.random() * max)));
 
 const MESSAGES = [
   'Всё отлично!',
@@ -23,36 +23,40 @@ const NAMES = [
   'Андрей',
   'Алексей',
   'Александр'
-]
+];
 
-let photosId = generateArray(25,25);
-let usersId = generateArray(25,25);
+const photosId = generateArray(25,25);
+const usersId = generateArray(25,25);
 
 function getId () {
-  let temp = usersId[getRandomPositiveInteger(0,usersId.length-1)];
+  const temp = usersId[getRandomPositiveInteger(0,usersId.length-1)];
   delete(usersId[getRandomPositiveInteger(0,usersId.length-1)]);
-  return temp
-};
+  return temp;
+}
 
-console.log(getId);
+
 function getPicId () {
-  let temp = photosId[getRandomPositiveInteger(0,photosId.length-1)];
+  const temp = photosId[getRandomPositiveInteger(0,photosId.length-1)];
   delete(photosId[getRandomPositiveInteger(0,photosId.length-1)]);
-  return temp
+  return temp;
 };
 
 
-let comment = {
+const comment = {
   id: getId(),
   avatar: `img/avatar-${ getRandomPositiveInteger(0, 5) }.svg`,
   message: MESSAGES[getRandomPositiveInteger(0, 1)],
   name: NAMES[getRandomPositiveInteger(0,NAMES.length-1)]
-}
+};
 
-let photoInfo = {
+const photoInfo = {
   id: getPicId(),
   url: `photos/${getPicId()}.jpg`,
   description: 'Описание фотографии',
   likes: `Количество лайков: ${getRandomPositiveInteger(15, 200)}`,
   comments: MESSAGES[getRandomPositiveInteger(0, 1)]
-}
+};
+
+console.log(photoInfo);
+console.log(comment)
+checkStringLength('123123',2313);
