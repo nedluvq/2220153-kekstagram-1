67ab5330998361645ac6a2ffb013619a43674fb5
nedluvq <vasav7001@gmail.com>
2022-10-04 19:29:12 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 function getRandomPositiveInteger (a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -6,7 +5,6 @@ function getRandomPositiveInteger (a, b) {
   return Math.floor(result);
 }
 
-// eslint-disable-next-line no-unused-vars
 function checkStringLength (checkingString, maxLength) {
   return checkingString.length <= maxLength;
 
@@ -46,6 +44,21 @@ function getPicId () {
 }
 
 // eslint-disable-next-line no-unused-vars
+const comment = {
+  id: getId(),
+  avatar: `img/avatar-${ getRandomPositiveInteger(0, 5) }.svg`,
+  message: MESSAGES[getRandomPositiveInteger(0, 1)],
+  name: NAMES[getRandomPositiveInteger(0,NAMES.length-1)]
+};
+// eslint-disable-next-line no-unused-vars
+const photoInfo = {
+  id: getPicId(),
+  url: `photos/${getPicId()}.jpg`,
+  description: 'Описание фотографии',
+  likes: `Количество лайков: ${getRandomPositiveInteger(15, 200)}`,
+  comments: MESSAGES[getRandomPositiveInteger(0, 1)]
+};
+
 checkStringLength('123123',2313);
 
 for(let i = 0; i <= 25;i++) {
@@ -57,10 +70,9 @@ for(let i = 0; i <= 25;i++) {
     comments: MESSAGES[getRandomPositiveInteger(0, 1)]
   })
   comments.push({
-      id: getPicId(),
-      url: `photos/${getPicId()}.jpg`,
-      description: 'Описание фотографии',
-      likes: `Количество лайков: ${getRandomPositiveInteger(15, 200)}`,
-      comments: MESSAGES[getRandomPositiveInteger(0, 1)]
+    id: getId(),
+    avatar: `img/avatar-${ getRandomPositiveInteger(0, 5) }.svg`,
+    message: MESSAGES[getRandomPositiveInteger(0, 1)],
+    name: NAMES[getRandomPositiveInteger(0,NAMES.length-1)]
   })
 }
