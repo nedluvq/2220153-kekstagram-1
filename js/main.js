438770571ee03f63cbd17ebd7a64cc5cff518b51
nedluvq <vasav7001@gmail.com>
2022-10-04 17:05:57 +0500

    task 4.15
    refactored + completed 4.15

--- js/main.js
+++ js/main.js
@@ -1,21 +1,58 @@
-function randomNumber(from,to) {
-  if(from < 0 || to < 0) {
-    return 'Одна из границ отрицательная';
-  }
-  if(from > to) {
-    return Math.random(to,from);
-  }
-  return Math.random(from,to);
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
-function checkStringLength(checkingString,maxLength ) {
-  if(checkingString.length <= maxLength ) {
-    return true;
-  }else{
-    return false;
-  }
+function checkStringLength (checkingString, maxLength) {
+  return checkingString.length <= maxLength
+
 }
 
-randomNumber(1,100);
-checkStringLength('123',123);
-checkStringLength('121231332',123);
+const generateArray = (length, max) => (
+  [...new Array(length)]
+    .map(() => Math.round(Math.random() * max)))
+
+const MESSAGES = [
+  'Всё отлично!',
+  'Неплохо, но не все'
+];
+
+const NAMES = [
+  'Андрей',
+  'Алексей',
+  'Александр'
+]
+
+let photosId = generateArray(25,25);
+let usersId = generateArray(25,25);
+
+function getId () {
+  let temp = usersId[getRandomPositiveInteger(0,usersId.length-1)];
+  delete(usersId[getRandomPositiveInteger(0,usersId.length-1)]);
+  return temp
+};
+
+console.log(getId);
+function getPicId () {
+  let temp = photosId[getRandomPositiveInteger(0,photosId.length-1)];
+  delete(photosId[getRandomPositiveInteger(0,photosId.length-1)]);
+  return temp
+};
+
+
+let comment = {
+  id: getId(),
+  avatar: `img/avatar-${ getRandomPositiveInteger(0, 5) }.svg`,
+  message: MESSAGES[getRandomPositiveInteger(0, 1)],
+  name: NAMES[getRandomPositiveInteger(0,NAMES.length-1)]
+}
+
+let photoInfo = {
+  id: getPicId(),
+  url: `photos/${getPicId()}.jpg`,
+  description: 'Описание фотографии',
+  likes: `Количество лайков: ${getRandomPositiveInteger(15, 200)}`,
+  comments: MESSAGES[getRandomPositiveInteger(0, 1)]
+}
